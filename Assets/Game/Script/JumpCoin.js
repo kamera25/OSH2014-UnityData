27f0ee0ee9fd0aee3de3fd6637d9a267a386aa3a
Takahiro Nakaoku <giraffe_rakugaki@yahoo.co.jp>
2014-01-22 14:35:33 +0900

    Upload Presantation and Game.
    Upload Coin Game , Presentation Data.

---
+++ Assets/Game/Script/JumpCoin.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+	if( Input.GetMouseButtonDown(0))
+	{
+		this.rigidbody.AddForceAtPosition( Vector3.up * 15, Vector3.forward, ForceMode.Impulse);
+	}
+}
---
+++ Assets/Presen/CameraRotate.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+private var degree: int = 0;
+private var nowdegree : int = 0;
+
+function Start () {
+
+}
+
+function Update () {
+	if( Input.GetMouseButtonDown(0))
+	{
+		degree += 90;
+	}
+	
+	
+	if( nowdegree < degree){
+		 this.transform.Rotate( 0, 2, 0);
+		 nowdegree += 2;
+	}
+}
